399f50774c0a0840c585aef5aec9328109a61d51
ShGulnaz <shakirova139@gmail.com>
2021-06-29 21:22:50 +0300

    fix rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,10 +2,6 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth/authReducer";
 import lessonReducer from "./lesson/lessonReducer";
 
-import { enableMapSet } from 'immer'
-
-enableMapSet()
-
 const rootReducer = combineReducers({
     authReducer,
     lessonReducer
