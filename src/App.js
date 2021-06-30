edeb2704b11e506677594658677d65787bc4187e
ShGulnaz <shakirova139@gmail.com>
2021-06-30 14:04:52 +0300

    fix private route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-// import PrivateRoute from "./components/PrivateRouter";
-import AuthPage from "./pages/AuthPage"
+import PrivateRoute from "./components/PrivateRoute";
+import { CalendarPage } from "./pages/CalendarPage";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import "./styles/globalStyle.css"
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={AuthPage} exact />
-        {/* <PrivateRoute path="/main" component={MainPage} /> */}
+        <Route path="/" component={SignIn} exact />
+          <Route path="/signup" component={SignUp} exact />
+          {/* <Route path="/calendar" component={CalendarPage} exact /> */}
+          <PrivateRoute path="/calendar" exact component={CalendarPage} />
       </Switch>
     </BrowserRouter>
   )
